6cc0314167d2479fe070d0ebd592139a7782eba0
Simon <simonschwaiger1308@gmail.com>
2025-06-11 13:15:12 +0200

    Add nav2 and fix ws sourcing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 source /opt/ros/$ROS_DISTRO/setup.bash
-source /opt/$(ROS2_WS)/install/local_setup.bash
+if [[ "$ROS_DISTRO" == "noetic" ]]; then
+  source $ROS2_WS/devel/setup.bash
+else
+  source $ROS2_WS/install/local_setup.bash
+fi
 
 exec "$@"
